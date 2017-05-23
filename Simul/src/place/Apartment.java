6d40203badadfc1acfcde5cfe9eebab77a91ab68
Ferlgrand <efe.yukselen@ozu.edu.tr>
2017-05-24 00:13:20 +0300

    sa

---
+++ Simul/src/place/Apartment.java
@@ -0,0 +1,21 @@
+package place;
+
+public class Apartment extends Building {
+	
+	private String name;
+	private double value;
+	
+	public Apartment(String name, double value) {
+		super(name, value);
+		this.name = name;
+		this.value = value;
+	}
+	
+	public Building clone() {
+		Building newBuilding = new Apartment(name, value);
+		newBuilding.setInhabitants(this.getInhabitants());
+		newBuilding.setItems(this.getItems());
+		return newBuilding;
+	}
+
+}
